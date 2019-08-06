a37a68ea39acaf4f15b77192d3fc0161fa9bcaf9
João Paulo Figueira <joao.figueira@gmail.com>
2019-08-06 16:30:28 +0100

    Minor corrections

--- geo/geomath.py
+++ geo/geomath.py
@@ -14,18 +14,18 @@ def vec_haversine(lat1: np.ndarray,
     :param lon2: Array of destination longitudes in degrees
     :return: Array of distances in meters
     """
-    earth_radius = 6371000
+    earth_radius = 6378137.0
 
     rad_lat1 = np.radians(lat1)
     rad_lon1 = np.radians(lon1)
     rad_lat2 = np.radians(lat2)
     rad_lon2 = np.radians(lon2)
 
-    dlon = rad_lon2 - rad_lon1
-    dlat = rad_lat2 - rad_lat1
+    d_lon = rad_lon2 - rad_lon1
+    d_lat = rad_lat2 - rad_lat1
 
-    a = np.sin(dlat/2.0)**2 + np.cos(rad_lat1) * np.cos(rad_lat2) \
-        * np.sin(dlon/2.0)**2
+    a = np.sin(d_lat/2.0)**2 + np.cos(rad_lat1) * np.cos(rad_lat2) \
+        * np.sin(d_lon/2.0)**2
 
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1.0 - a))
     meters = earth_radius * c
@@ -44,18 +44,18 @@ def num_haversine(lat1: float,
     :param lon2: Destination longitude in degrees
     :return: Distances in meters
     """
-    earth_radius = 6371000
+    earth_radius = 6378137.0
 
     rad_lat1 = math.radians(lat1)
     rad_lon1 = math.radians(lon1)
     rad_lat2 = math.radians(lat2)
     rad_lon2 = math.radians(lon2)
 
-    dlon = rad_lon2 - rad_lon1
-    dlat = rad_lat2 - rad_lat1
+    d_lon = rad_lon2 - rad_lon1
+    d_lat = rad_lat2 - rad_lat1
 
-    a = math.sin(dlat/2.0)**2 + math.cos(rad_lat1) * math.cos(rad_lat2) \
-        * math.sin(dlon/2.0)**2
+    a = math.sin(d_lat/2.0)**2 + math.cos(rad_lat1) * math.cos(rad_lat2) \
+        * math.sin(d_lon/2.0)**2
 
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1.0 - a))
     meters = earth_radius * c
